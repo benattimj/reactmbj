035ba0ffa59e4e596c057464859eee9d5647d135
Murilo Benatti <benatti1104@gmail.com>
2023-01-06 21:47:08 -0300

    Update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/CV.pdf'
+import TX from '../../assets/TX.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download>   Download CV </a>
+        <a href={TX} download>   Download CV </a>
       <a href="https://api.whatsapp.com/send/?phone=5534991286927&text&type=phone_number&app_absent=0" classname='btn'> Let's talk </a>
 
     </div>
