57083f5cd0116ecfdf5a62a89dbd6c56e72cbc9d
benattimj <benatti1104@gmail.com>
2024-07-08 16:56:28 -0300

    x

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2>Contact Mee</h2>
 
       <div className="container contact_container">
         <div className='contact_options'>
