9d2aaea9ad8c77cc8a720eaec35521845c201a40
benattimj <benatti1104@gmail.com>
2024-07-08 17:17:07 -0300

    Resume some services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './services.css'
-import {FiCheck} from 'react-icons/fi'
+import { FiCheck } from 'react-icons/fi'
 
 const Services = () => {
   return (
@@ -9,105 +9,97 @@ const Services = () => {
       <h2>Services</h2>
 
 
-<div className="container services_container">
+      <div className="container services_container">
 
-    <article className="service"> 
-  <div className="service_head">
-    <h3> UI/UX Design</h3>
-</div>
+        <article className="service">
+          <div className="service_head">
+            <h3> UI/UX Design</h3>
+          </div>
 
-<ul className='service_list'>
-   <li>
-      <FiCheck className='service_list-icon'/>
-        <p>The art of structure is in the beauties.</p>  
-    </li>
-    <li>
-      <FiCheck className='service_list-icon'/>
-        <p>Our inspiration is in the idea of the ​​innovating, of shining we need energy.</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p>Creating applications with the best visuals, real structures, absolute control.</p>  
-    </li>
-     <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Lorem, ipsum dolor sit amet constecteur adipiscing</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Lorem, ipsum dolor sit amet constecteur adipiscing</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Lorem, ipsum dolor sit amet constecteur adipiscing</p>  
-    </li>
-    </ul>
-    </article>
-    {/* END OF UI/UX */}
+          <ul className='service_list'>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p>The art of structure is in the beauties.</p>
+            </li>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p>Our inspiration is in the idea of the ​​innovating, of shining we need energy.</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p>Creating applications with the best visuals, real structures, absolute control.</p>
+            </li>
 
- <article className="service"> 
-  <div className="service_head">
-    <h3> Web Development</h3>
-</div>
 
-<ul className='service_list'>
-   <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Systems of Products, Clients, Storage, Financials, sales </p>  
-    </li>
-    <li>
-      <FiCheck className='service_list-icon'/>
-        <p>The latest, most advanced and fastest technology on the market</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Support, update and renews on your application.</p>  
-    </li>
-     <li>
-      <FiCheck className='service_list-icon'/>
-        <p> The idea its not be limited</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Practical, easy to access, good reading and many sales</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Thats sounds perfect for you</p>  
-    </li>
-    </ul>
-    </article>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
 
-    {/* WEB Development X */}
+        <article className="service">
+          <div className="service_head">
+            <h3> Web Development</h3>
+          </div>
 
-    <article className="service"> 
-  <div className="service_head">
-    <h3> Content Creation</h3>
-</div>
+          <ul className='service_list'>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p> Systems of Products, Clients, Storage, Financials, sales </p>
+            </li>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p>The latest, most advanced and fastest technology on the market</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> Support, update and renews on your application.</p>
+            </li>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p> The idea its not be limited</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> Practical, easy to access, good reading and many sales</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> Thats sounds perfect for you</p>
+            </li>
+          </ul>
+        </article>
 
-<ul className='service_list'>
-   <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Web Store systems</p>  
-    </li>
-    <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Portfolio applications</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Control of products application</p>  
-    </li>
-     <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Hotel Systems</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> Financial control system</p>  
-    </li> <li>
-      <FiCheck className='service_list-icon'/>
-        <p> E-commerce</p>  
-    </li>
-    </ul>
+        {/* WEB Development X */}
 
-    </article>
+        <article className="service">
+          <div className="service_head">
+            <h3> Content Creation</h3>
+          </div>
 
-    
-    {/* END Of Creation X */}
-</div>
+          <ul className='service_list'>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p> Web Store systems</p>
+            </li>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p> Portfolio applications</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> Control of products application</p>
+            </li>
+            <li>
+              <FiCheck className='service_list-icon' />
+              <p> Hotel Systems</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> Financial control system</p>
+            </li> <li>
+              <FiCheck className='service_list-icon' />
+              <p> E-commerce</p>
+            </li>
+          </ul>
+
+        </article>
+
+
+        {/* END Of Creation X */}
+      </div>
     </section>
   )
 }
