3ddd6a9f4ee8337d6f1c6c3fc029583fbb9597b3
benattimj <benatti1104@gmail.com>
2024-07-08 16:47:13 -0300

    API

---
+++ README.md
@@ -0,0 +1,18 @@
+# My Porfolio
+
+Build & Deploy React JS Portfolio Website [ FULL RESPONSIVE UI/UX ]
+
+The Website : https://mbjportofolio.vercel.app/
+
+
+pt1
+
+![b1](https://user-images.githubusercontent.com/95152221/211711373-9fca4a20-d36c-4b71-9820-a0f6862565ab.jpg)
+
+pt2
+
+![b2](https://user-images.githubusercontent.com/95152221/211711471-5d2cdb46-5cd5-47b0-a69e-93ca3e7410a9.jpg)
+
+pt3
+
+![end](https://user-images.githubusercontent.com/95152221/211711506-31c0a4b8-9383-4be4-9351-e8373a746fd5.jpg)
---
+++ build/precache-manifest.ffe4b636e3c20283cbe22b8a8eb5d845.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3646dd8618569c85d9e9",
+    "url": "react-portfolio-website/static/css/main.a17dcc32.chunk.css"
+  },
+  {
+    "revision": "3646dd8618569c85d9e9",
+    "url": "react-portfolio-website/static/js/main.322d1739.chunk.js"
+  },
+  {
+    "revision": "cf5f6beff65370ef7fb5",
+    "url": "react-portfolio-website/static/js/runtime~main.903281f4.js"
+  },
+  {
+    "revision": "fb389c320fd81a485312",
+    "url": "react-portfolio-website/static/css/2.a219702e.chunk.css"
+  },
+  {
+    "revision": "fb389c320fd81a485312",
+    "url": "react-portfolio-website/static/js/2.9e756f26.chunk.js"
+  },
+  {
+    "revision": "54851c03e8c55a04e2a4d2ce152d835e",
+    "url": "react-portfolio-website/static/media/CV.54851c03.pdf"
+  },
+  {
+    "revision": "0f2151e440f71446333d3e9089a2f650",
+    "url": "react-portfolio-website/static/media/Mbj.0f2151e4.jpg"
+  },
+  {
+    "revision": "a94e1eb2559263fdc284cb06de184e9e",
+    "url": "react-portfolio-website/static/media/euuu.a94e1eb2.jpg"
+  },
+  {
+    "revision": "6a33cd6ad57e35cd92f8091e6add55e9",
+    "url": "react-portfolio-website/static/media/pg1.6a33cd6a.jpg"
+  },
+  {
+    "revision": "2e061e93a85691536b2827016fd74a0e",
+    "url": "react-portfolio-website/static/media/pg2.2e061e93.jpg"
+  },
+  {
+    "revision": "db358aa3ae66e5e5ba5cfdbf2377739c",
+    "url": "react-portfolio-website/static/media/pg3.db358aa3.png"
+  },
+  {
+    "revision": "aeffb30c4455e4a5827db4e8e9a63da8",
+    "url": "react-portfolio-website/static/media/pg5.aeffb30c.png"
+  },
+  {
+    "revision": "43522b44921d34d591ae6588860f2b6c",
+    "url": "react-portfolio-website/static/media/pg6.43522b44.jpg"
+  },
+  {
+    "revision": "7cf81bf6b7c7257239d3f89a854e0d9e",
+    "url": "react-portfolio-website/static/media/avtr3.7cf81bf6.jpg"
+  },
+  {
+    "revision": "a39c9e5dbe0e0131a260e2e75e093220",
+    "url": "react-portfolio-website/static/media/avtr4.a39c9e5d.jpg"
+  },
+  {
+    "revision": "b3238cca469bd256dac864c04351897f",
+    "url": "react-portfolio-website/static/media/fundoo.b3238cca.jpg"
+  },
+  {
+    "revision": "278679e4b079857429f4582f6264b8dc",
+    "url": "react-portfolio-website/index.html"
+  }
+];
